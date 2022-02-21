81149aa3eb510be3685725e1a84b58104c3e8559
Iñigo <igutierrezzz78@gmail.com>
2022-02-21 20:22:54 +0100

    proyecto empezado

--- src/FestivalesIO.java
+++ src/FestivalesIO.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
  * La clase contiene méodos estáticos que permiten
  * cargar la agenda de festivales leyendo los datos desde
  * un fichero
+ * @author Iñigo Gutierrez
  */
 public class FestivalesIO {
 
