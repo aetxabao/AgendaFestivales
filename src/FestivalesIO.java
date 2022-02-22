6a837cc2444ece407d8bbb711d7304022f93f953
agarciagale <agarciagale@gmail.com>
2022-02-22 19:46:36 +0100

    Terminada parte a) de proyecto UT6 Festivales

--- src/FestivalesIO.java
+++ src/FestivalesIO.java
@@ -1,13 +1,17 @@
 
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
 
 /**
  * La clase contiene méodos estáticos que permiten
  * cargar la agenda de festivales leyendo los datos desde
  * un fichero
+ *
+ * @author Adrian Garcia Galera
+ *
  */
 public class FestivalesIO {
 
@@ -37,10 +41,65 @@ public class FestivalesIO {
      * @return el festival creado
      */
     public static Festival parsearLinea(String lineaFestival) {
-       //TODO
-        
-        return null;
+
+        Festival festival = new Festival(getNombre(lineaFestival), getLugar(lineaFestival),
+                getFecha(lineaFestival), getDuracion(lineaFestival), getEstilos(lineaFestival));
+        return festival;
+
     }
+
+    public static String getNombre(String str){
+        String nombre = str.substring(0,str.indexOf(':'));
+        nombre = nombre.trim();
+
+        String palabras[] = nombre.split("\\s");
+        String nombreFinal = "";
+
+        for (String palabra : palabras) {
+            String inicial = palabra.substring(0,1);
+            String resto = palabra.substring(1);
+
+            nombreFinal+= inicial.toUpperCase() + resto + " ";
+        }
+        return nombreFinal.trim();
+    }
+
+    public static String getLugar(String str){
+        String palabras[] = str.split(":");
+        String lugar = palabras[1];
+        lugar = lugar.toUpperCase();
+        return lugar.trim();
+    }
+
+    public static LocalDate getFecha(String str){
+        String palabras[] = str.split(":");
+        String fecha = palabras[2];
+        fecha = fecha.trim();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate localDate = LocalDate.parse(fecha, formatter);
+        return localDate;
+    }
+
+    public static int getDuracion(String str){
+        String palabras[] = str.split(":");
+        String duracion = palabras[3];
+        duracion = duracion.trim();
+        return Integer.parseInt(duracion);
+    }
+
+    public static HashSet<Estilo> getEstilos(String str){
+        String palabras[] = str.split(":");
+        HashSet<Estilo> estilos = new HashSet<>();
+
+        for (int i = 4; i < palabras.length; i++) {
+            palabras[i] = palabras[i].trim();
+            estilos.add(Estilo.valueOf(palabras[i].toUpperCase()));
+        }
+        return estilos;
+    }
+
+
+
     
    
     
