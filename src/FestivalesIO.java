038f3fa5b6e05da85bdc0dd3d509127bacfef7dd
jgarciapedr <jgarciapedr@gmail.com>
2022-02-22 20:00:10 +0100

    Autor

--- src/FestivalesIO.java
+++ src/FestivalesIO.java
@@ -8,6 +8,9 @@ import java.util.Scanner;
  * La clase contiene méodos estáticos que permiten
  * cargar la agenda de festivales leyendo los datos desde
  * un fichero
+ *
+ * @author Joan García Pedraza
+ *
  */
 public class FestivalesIO {
 
